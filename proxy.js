96478319736e41df3fd65e120b7519c6faa4375f
Charlotte Gore <conspiracygore@gmail.com>
2015-01-26 13:00:01 +0000

    Added error detection

--- proxy.js
+++ proxy.js
@@ -11,89 +11,96 @@ var spaql;
 
 module.exports.configure = function (hostname, port){
   //sparql = new SparkleSparkleGo('ld.local', 8030, );
+  console.log('Using ' + hostname + ':' + port + ' as linked data service');
   sparql = new SparkleSparkleGo(hostname, port, '/sparql/query{?query*}');
   
 }
 
 module.exports.listen = function (app){
 
-  var 
+  console.log('Attempting to connect...');
 
   // get the initial list of concepts... 
   sparql
     .query(queries.tagsForType('nice:Recommendation'))
     .execute(parseTriples(function (err, tx){
 
-      console.log('Ready to serve concepts')
+      if (!err){
 
-      var ts = new TrieSearch();
+        console.log('Ready to serve concepts')
 
-      tx.forEach(function (triple){
+        var ts = new TrieSearch();
 
-          // strip the quotation marks
-          var object = triple.object.replace(/\"/g, ''); 
-          var subject = triple.subject;
+        tx.forEach(function (triple){
 
-          var temp = {};
-          temp[object.toLowerCase()] = {object : object, subject : subject};
+            // strip the quotation marks
+            var object = triple.object.replace(/\"/g, ''); 
+            var subject = triple.subject;
 
-          ts.addFromObject(temp);
+            var temp = {};
+            temp[object.toLowerCase()] = {object : object, subject : subject};
 
-      });
+            ts.addFromObject(temp);
 
-      app.get('/ld/concepts/:term', function (req, res){
-
-        var term = req.params.term.toLowerCase();
-
-        var results = ts.get(term);
-
-        // sort alphabetically;
-        results = results.sort(function (a, b){
-          // shortest term first...
-          if (a._key_ < b._key){
-            return -1;
-          } else if (a._key_ > b._key_){
-            return 1;
-          } else {
-            return 0;
-          }
-          
         });
 
-        results = results.sort(function (a, b){
-          if (a._key_.indexOf(term) < b._key_.indexOf(term)){
-            return -1;
-          } else if (a._key_.indexOf(term) > b._key_.indexOf(term)){
-            return 1;
-          } else {
-            return 0;
-          }
+        app.get('/ld/concepts/:term', function (req, res){
+
+          var term = req.params.term.toLowerCase();
+
+          var results = ts.get(term);
+
+          // sort alphabetically;
+          results = results.sort(function (a, b){
+            // shortest term first...
+            if (a._key_ < b._key){
+              return -1;
+            } else if (a._key_ > b._key_){
+              return 1;
+            } else {
+              return 0;
+            }
+            
+          });
+
+          results = results.sort(function (a, b){
+            if (a._key_.indexOf(term) < b._key_.indexOf(term)){
+              return -1;
+            } else if (a._key_.indexOf(term) > b._key_.indexOf(term)){
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+
+          res.send(results.slice(0,5));
+
         });
 
-        res.send(results.slice(0,5));
+        app.get('/ld/recommendations/:tag', function (req, res){
 
-      });
+          if (!cache[req.params.tag]){
 
-      app.get('/ld/recommendations/:tag', function (req, res){
+            sparql
+              .query(queries.contentMatching('nice:Recommendation', req.params.tag))
+              .execute(parseTriples(function (err, triples){
 
-        if (!cache[req.params.tag]){
+                if (!err){
+                  cache[req.params.tag] = triples;
+                  res.send(triples);
+                }
 
-          sparql
-            .query(queries.contentMatching('nice:Recommendation', req.params.tag))
-            .execute(parseTriples(function (err, triples){
+              }));
 
-              if (!err){
-                cache[req.params.tag] = triples;
-                res.send(triples);
-              }
+          } else {
+            res.send(cache[req.params.tag]);
+          }
 
-            }));
+        });
 
-        } else {
-          res.send(cache[req.params.tag]);
-        }
-
-      });
+      } else {
+        console.error('Request error', err);
+      }
 
     }));
 
